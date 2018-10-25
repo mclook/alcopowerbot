8eed7c2ad32f3383c9afb11e060e76d4d93768eb
mclook <44475053+mclook@users.noreply.github.com>
2018-10-25 20:08:32 +0300

    initial

---
+++ index.js
@@ -0,0 +1,8 @@
+const TelegramBot=require('node-telegram-bot-api')
+const TOKEN='654555427:AAHHyDDjZTrwb6oC0FXo0l446NRzhqDosMc'
+
+const bot=new TelegramBot(TOKEN,{polling:true})
+
+bot.on('message',msg=>{
+    bot.sendMessage(msg.chat.id,'Darova')
+})
