f8435f5c0e4f16a9f7f90c84dd62fd47e55f92df
mclook <44475053+mclook@users.noreply.github.com>
2018-10-25 21:16:01 +0300

    initial

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const TOKEN='654555427:AAHHyDDjZTrwb6oC0FXo0l446NRzhqDosMc'
 
 const bot=new TelegramBot(TOKEN,{polling:true})
 
-bot.onText(/бухло/,msg=>{
+bot.onText('что пить',msg=>{
+
     bot.sendMessage(msg.chat.id,'пить')
     bot.sendPhoto(msg.chat.id,'https://avatanplus.com/files/resources/original/588dd50e40b89159ea083fb9.png')
+
 })
 
 require('http').createServer().listen(process.env.PORT || 5000).on('request', function(req, res){
