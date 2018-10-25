6ba44f42a0ddb234c173470a04e2ed49495556a2
mclook <44475053+mclook@users.noreply.github.com>
2018-10-25 21:02:15 +0300

    initial

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const TelegramBot=require('node-telegram-bot-api')
 const TOKEN='654555427:AAHHyDDjZTrwb6oC0FXo0l446NRzhqDosMc'
 
 const bot=new TelegramBot(TOKEN,{polling:true})
+bot.onText(/привет/,msg=>{
+    bot.sendMessage(msg.chat.id,'Привет')
+})
 
-bot.on('.message',msg=>{
+bot.on('message',msg=>{
+
     bot.sendMessage(msg.chat.id,'Da')
 })
 require('http').createServer().listen(process.env.PORT || 5000).on('request', function(req, res){
     res.end('')
 })
-bot.
+
